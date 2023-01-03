39a20b39d8b3fd6c625e1cfb76fb9d58753d82ea
Imran Shah <is9149933764@gmail.com>
2023-01-03 23:17:25 +0530

    commit

--- src/Components/AddRet.js
+++ src/Components/AddRet.js
@@ -4,7 +4,7 @@ const AddRet = () => {
   return (
     <div className="flex p-4 lg:w-[30vw] sm:w-[10vw] ml-12 mt-10 gap-3 gap-y-4 ">
       <button className=" p-4 bg-blue-500 text-white">
-        <Link to={"/"}></Link>
+        <Link to={"/"}>Add New Person</Link>
       </button>
       <button className="p-4 bg-blue-500 text-white">
         <Link to={"/retrieve"}>Retrieve Information</Link>
